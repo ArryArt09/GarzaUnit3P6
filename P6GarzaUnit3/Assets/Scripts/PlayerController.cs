9f151de5b54e8fc06a6dd38ba109a576c2094086
ArryArt09 <746785@student.dusd.net>
2026-03-31 13:53:03 -0700

    Play
    aadd animattionsd MAKE M  SUIC

--- P6GarzaUnit3/Assets/Scripts/PlayerController.cs
+++ P6GarzaUnit3/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody playerRb;
     public float jumpForce = 10;
+    public float overFlyForce = 100;
     public float gravityModifier;
 
     public bool isOnGround = true;
+
+    public bool gameOver = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,10 +27,23 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
         }
+        if (gameOver)
+        {
+            isOnGround = false;
+            playerRb.AddForce(Vector3.left * overFlyForce, ForceMode.Impulse);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
     }
 }
